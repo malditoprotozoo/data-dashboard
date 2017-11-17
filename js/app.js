5ad3a5751c511b40a70034d47fb6f2d73b205c3f
malditoprotozoo <virodriguez.fe@gmail.com>
2017-11-17 14:53:23 -0300

    Overview Structure ready. I also made some tweaks to the JS

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
-/*
- * Funcionalidad de tu producto
- */
-
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+
+// Creando las variables necesarias
+var city = document.getElementById("city");
+
+
+
+
+
+
+
+// var container = document.getElementById('container');
+// console.log(container);
+
+// for (var i = 0; i < data.SCL['2016-2'].students.length; i++) {
+// 	console.log(data.SCL['2016-2'].students[i].name);
+// 	var parrafo = document.createElement('p');
+// 	var nodoParrafo = document.createTextNode('La estudiante número ' + [i+1] + ' es: ' + data.SCL['2016-2'].students[i].name);
+// 		parrafo.appendChild(nodoParrafo);
+// 		container.appendChild(parrafo);
+// };
 
 // google.charts.load('current', {'packages':['corechart']});
 // google.charts.setOnLoadCallback(drawChart);
