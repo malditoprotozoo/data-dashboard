743916848759b0eee1bf785e5e49a330b00adce1
malditoprotozoo <virodriguez.fe@gmail.com>
2017-11-17 09:32:23 -0300

    Some divs added to the content space

--- js/app.js
+++ js/app.js
@@ -4,3 +4,25 @@
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+
+// google.charts.load('current', {'packages':['corechart']});
+// google.charts.setOnLoadCallback(drawChart);
+
+// function drawChart() {
+//   var data = google.visualization.arrayToDataTable([
+//   ['Task', 'Hours per Day'],
+//   ['Work', 8],
+//   ['Friends', 2],
+//   ['Eat', 2],
+//   ['TV', 3],
+//   ['Gym', 2],
+//   ['Sleep', 7]
+// ]);
+
+//   // Optional; add a title and set the width and height of the chart
+//   var options = {'title':'My Average Day', 'width':700, 'height':500};
+
+//   // Display the chart inside the <div> element with id="piechart"
+//   var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+//   chart.draw(data, options);
+// }
